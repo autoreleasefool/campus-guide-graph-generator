236108cc875bfd06be4efa6a4d406f4a9fe379bd
Joseph Roque <contact@josephroque.ca>
2017-03-22 12:31:24 -0400

    Load and draw image

--- js/script.js
+++ js/script.js
@@ -1,66 +1,209 @@
-const TOOLBAR_WIDTH = 300;
+/************************************************
+ * VARIABLES
+ ************************************************/
 
-// List of all canvases
-const canvases = [];
-// For drawing the background
-let backgroundCanvas;
+// The list of floors
+const floors = [
+  {
+    name: '',
+    img: null,
+    imgName: '',
+    nodes: [],
+    edges: [],
+  }
+];
+// The floor currently being edited
+let currentFloor = 0;
+
+// List of available tools
+const tools = [
+  '#tool-select',
+  '#tool-pan',
+  '#tool-add',
+  '#tool-remove',
+  '#tool-edge',
+  '#tool-zoom-in',
+  '#tool-zoom-out',
+];
+// Currently selected tool
+let currentTool = 1;
+
+// List of available menus
+const menus = [
+  "#base-props",
+  "#node-props",
+  "#edge-props",
+];
+// Currently shown menu
+let currentMenu = 0;
+
+// Width of the properties panel
+const PROPERTIES_PANEL_WIDTH = 300;
+
 // For drawing the user generated content
-let contentCanvas;
-// For drawing the toolset
-let toolCanvas;
+let canvas;
+// Graphics context for the canvas
+let canvasCtx;
 
 // Width of the canvases
 let canvasWidth = 0;
 // Height of the canvases
 let canvasHeight = 0;
 
-/*
+/************************************************
  * TOOLS
+ ************************************************/
+
+/**
+ * Adds a border around the currently selected tool, removes border around any others.
  */
-
-function renderToolbar() {
-
+function highlightCurrentTool() {
+  for (let i = 0; i < tools.length; i++) {
+    if (i === currentTool) {
+      $(tools[i]).addClass('selected-icon');
+    } else {
+      $(tools[i]).removeClass('selected-icon');
+    }
+  }
 }
 
-/*
- * CONTENT
- */
+/************************************************
+ * PROPERTIES
+ ************************************************/
 
-/*
- * BACKGROUND
+/**
+ * Sets the image of the floor.
+ *
+ * @param {number} floor the floor to set the image path of
+ * @param {string} name  name of the image
+ * @param {any}    img   image
  */
+function setFloorImage(floor, name, img) {
+  floors[floor].imgName = name
+  floors[floor].img = img;
+  $('#base-floor-image').val(name);
+}
 
-/*
- * GENERAL
+/************************************************
+ * MENUS
+ ************************************************/
+
+function showCurrentMenu() {
+  for (let i = 0; i < menus.length; i++) {
+    if (i === currentMenu) {
+      $(menus[i]).removeClass("hidden-prop");
+    } else {
+      $(menus[i]).addClass("hidden-prop");
+    }
+  }
+
+  populateMenu(currentMenu);
+}
+
+/**
+ * Populates data in the menu.
+ *
+ * @param {number} menu the menu to populate
  */
+function populateMenu(menu) {
+  switch (menu) {
+    case 0: /* Base properties */
+      break;
+    case 1: /* Node properties */
+      break;
+    case 2: /* Edge properties */
+      break;
+    default: /* Nothing */
+      break;
+  }
+}
+
+/************************************************
+ * CANVAS
+ ************************************************/
+
+function redraw() {
+  const floor = currentFloor;
+  canvasCtx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+  if (floors[floor].img) {
+    canvasCtx.drawImage(floors[floor].img, 0, 0);
+  }
+}
 
 /**
  * Handle when the window is resized by resizing the canvases accordingly.
  */
 function handleResize() {
-  canvasWidth = document.body.clientWidth - TOOLBAR_WIDTH;
+  canvasWidth = document.body.clientWidth - PROPERTIES_PANEL_WIDTH;
   canvasHeight = document.body.clientHeight;
+  canvas.css('width', `${canvasWidth}px`)
+      .css('height', `${canvasHeight}px`)
+      .attr('width', `${canvasWidth}px`)
+      .attr('height', `${canvasHeight}px`);
+  redraw();
+}
 
-  for (let i = 0; i < canvases.length; i++) {
-    canvases[i].css("width", `${canvasWidth}px`);
-    canvases[i].css("height", `${canvasHeight}px`);
+/************************************************
+ * FILE I/O
+ ************************************************/
+
+/**
+ * Handles a new floor image being set
+ *
+ * @param {any} e the change event
+ */
+function handleFloorImage(e) {
+  const reader = new FileReader();
+  reader.onload = function(event) {
+    const img = new Image();
+    img.onload = function() {
+      setFloorImage(currentFloor, e.target.files[0].name, img);
+      redraw();
+    }
+    img.src = event.target.result;
+  }
+
+  if (/image\/(png|jpe?g)/g.test(e.target.files[0].type)) {
+    reader.readAsDataURL(e.target.files[0]);
+  } else {
+    alert(`You\'ve selected an invalid filetype: ${e.target.files[0].type}`);
   }
 }
 
+/************************************************
+ * EVENTS
+ ************************************************/
+
 // On ready
 $(document).ready(function() {
-  // Get references to each canvas and store
-  backgroundCanvas = $('#backgroundCanvas');
-  contentCanvas = $('#contentCanvas');
-  toolCanvas = $('#toolCanvas');
-
-  canvases.push(backgroundCanvas);
-  canvases.push(contentCanvas);
-  canvases.push(toolCanvas);
+  canvas = $('#contentCanvas');
+  canvasCtx = document.getElementById('contentCanvas').getContext('2d');
   handleResize();
 
-  $("#toolbar").draggable({ containment: "#toolbar-area" });
+  showCurrentMenu();
+  highlightCurrentTool();
+
+  $('#toolbar').draggable({ containment: '#toolbar-area' });
+
+  // Handle selecting new tool
+  $('.tool').click(function() {
+    currentTool = tools.indexOf(`#${$(this).attr('id')}`);
+    highlightCurrentTool();
+  });
+
+  // Handle user selecting new image
+  document.getElementById('floor-image').addEventListener('change', handleFloorImage, false);
+  $('.prop-folder').click(function() {
+    document.getElementById($(this).attr('for')).click();
+  });
+
+  // Update floor name
+  $('#base-floor').on('input', (event) => {
+    floors[currentFloor].name = event.target.value;
+  })
+
 });
 
 // Bind resize function
-$(window).bind("resize", handleResize.bind(this));
+$(window).bind('resize', handleResize.bind(this));
