60e75cf37c7db94fc51386afcdcf559fa9595d23
Joseph Roque <contact@josephroque.ca>
2017-03-22 19:24:33 -0400

    Adding, removing and drawing nodes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
 /************************************************
- * VARIABLES
+ * CONSTANTS
+ ************************************************/
+
+// Width of the properties panel
+const PROPERTIES_PANEL_WIDTH = 300;
+
+// Radius of a node
+const NODE_SIZE = 4;
+
+const MENU_BASE = 0;
+const MENU_NODE = 1;
+const MENU_EDGE = 2;
+
+const TOOL_SELECT = 0;
+const TOOL_PAN = 1;
+const TOOL_ADD = 2;
+const TOOL_REMOVE = 3;
+const TOOL_EDGE = 4;
+const TOOL_ZOOM_IN = 5;
+const TOOL_ZOOM_OUT = 6;
+
+const NODE_TYPE_DOOR = 0;
+const NODE_TYPE_STAIRS = 1;
+const NODE_TYPE_ELEVATOR = 2;
+const NODE_TYPE_HALL = 3;
+const NODE_TYPE_ROOM = 4;
+
+/************************************************
+ * VARIABLES - GRAPH
  ************************************************/
 
 // The list of floors
@@ -26,7 +54,7 @@ const tools = [
   '#tool-zoom-out',
 ];
 // Currently selected tool
-let currentTool = 1;
+let currentTool = TOOL_PAN;
 
 // List of available menus
 const menus = [
@@ -35,10 +63,19 @@ const menus = [
   "#edge-props",
 ];
 // Currently shown menu
-let currentMenu = 0;
+let currentMenu = MENU_BASE;
 
-// Width of the properties panel
-const PROPERTIES_PANEL_WIDTH = 300;
+// Most recently assigned node type
+let mostRecentNodeType = NODE_TYPE_DOOR;
+
+// The currently selected node
+let selectedNode = null;
+// The currently selected edge
+let selectedEdge = null;
+
+/************************************************
+ * VARIABLES - CANVAS
+ ************************************************/
 
 // For drawing the user generated content
 let canvas;
@@ -63,6 +100,71 @@ function highlightCurrentTool() {
       $(tools[i]).addClass('selected-icon');
     } else {
       $(tools[i]).removeClass('selected-icon');
+    }
+  }
+}
+
+/**
+ * Tries to select a node, edge, or nothing.
+ *
+ * @param {number} x     x position of click
+ * @param {number} y     y position of click
+ * @param {number} floor the floor to select from
+ */
+function tryToSelectAt(x, y, floor) {
+  let selectedNode = null;
+  let selectedEdge = null;
+  let newMenu = null;
+
+  const nodes = floors[floor].nodes;
+  for (let i = 0; i < nodes.length; i++) {
+    if (Math.abs(nodes[i].x - x) <= NODE_SIZE && Math.abs(nodes[i].y - y) <= NODE_SIZE) {
+      selectedNode = nodes[i];
+      newMenu = MENU_NODE;
+      break;
+    }
+  }
+
+  if (selectedNode == null) {
+    const edges = floors[floor].edges;
+    for (let i = 0; i < edges.length; i++) {}
+  }
+
+  if (selectedNode == null && selectedEdge == null) {
+    newMenu = MENU_BASE;
+  }
+
+  setCurrentMenu(newMenu);
+  redraw();
+}
+
+/**
+ * Adds a new node to the graph.
+ *
+ * @param {number} x     x location of new node
+ * @param {number} y     y location of new node
+ * @param {number} floor floor to add new node to
+ * @param {number} type  type of new node
+ */
+function addNewNode(x, y, floor, type) {
+  const newNode = { x, y, type };
+  floors[floor].nodes.push(newNode);
+  return newNode;
+}
+
+/**
+ * Attempts to remove a node from the graph, if there is one at the location.
+ *
+ * @param {number} x     x location of node to remove
+ * @param {number} y     y location of node to remove
+ * @param {number} floor floor to remove node from
+ */
+function removeNode(x, y, floor) {
+  const nodes = floors[floor].nodes;
+  for (let i = 0; i < nodes.length; i++) {
+    if (Math.abs(nodes[i].x - x) <= NODE_SIZE && Math.abs(nodes[i].y - y) <= NODE_SIZE) {
+      nodes.splice(i, 1);
+      return;
     }
   }
 }
@@ -156,7 +258,20 @@ function updateFloorList() {
  * MENUS
  ************************************************/
 
-function showCurrentMenu() {
+/**
+ * Clears selection and shows base menu.
+ */
+function resetToBaseMenu() {
+  selectedEdge = null;
+  selectedNode = null;
+  setCurrentMenu(MENU_BASE);
+}
+
+/**
+ * Sets the current menu in the side bar, hides all others.
+ */
+function setCurrentMenu(menu) {
+  currentMenu = menu;
   for (let i = 0; i < menus.length; i++) {
     if (i === currentMenu) {
       $(menus[i]).removeClass("hidden-prop");
@@ -192,12 +307,108 @@ function populateMenu(menu) {
  * CANVAS
  ************************************************/
 
+/**
+ * Handles click on the canvas, based on the current tool
+ */
+function handleCanvasClick(event) {
+  switch(currentTool) {
+    default: // #tool-select
+      tryToSelectAt(event.offsetX, event.offsetY, currentFloor);
+      break;
+    case 1: // #tool-pan
+      break;
+    case 2: // #tool-add
+      selectedNode = addNewNode(event.offsetX, event.offsetY, currentFloor, mostRecentNodeType);
+      setCurrentMenu(MENU_NODE);
+      redraw();
+      break;
+    case 3: // #tool-remove
+      removeNode(event.offsetX, event.offsetY, currentFloor);
+      redraw();
+      resetToBaseMenu();
+      break;
+    case 4: // #tool-edge
+      break;
+    case 5: // #tool-zoom-in
+      break;
+    case 6: // #tool-zoom-out
+      break;
+  }
+}
+
+function handleCanvasMouseDown(event) {
+  switch (currentTool) {
+    case 1:
+      break;
+    case 4:
+      break;
+    default: break; // do nothing
+  }
+}
+
+function handleCanvasMouseUp(event) {
+  switch (currentTool) {
+    case 1:
+      break;
+    case 4:
+      break;
+    default: break; // do nothing
+  }
+}
+
+function handleCanvasMouseMove(event) {
+  switch (currentTool) {
+    case 1:
+      break;
+    case 4:
+      break;
+    default: break; // do nothing
+  }
+}
+
+/**
+ * Gets the stroke for a node type
+ *
+ * @param {number} type node type
+ */
+function getNodeTypeStrokeStyle(type) {
+  return 'black';
+}
+
+/**
+ * Gets the fill for a node type
+ *
+ * @param {number} type node type
+ */
+function getNodeTypeFillStyle(type) {
+  switch (type) {
+    case 0: return 'green';   // Doorway
+    case 1: return 'red';     // Staircase
+    case 2: return 'yellow';  // Elevator
+    case 3: return 'blue';    // Hallway
+    default: return 'black';  // Room
+  }
+}
+
+/**
+ * Clears the canvas and redraws image, nodes ,and edges
+ */
 function redraw() {
-  const floor = currentFloor;
+  const floor = floors[currentFloor];
   canvasCtx.clearRect(0, 0, canvasWidth, canvasHeight);
 
-  if (floors[floor].img) {
-    canvasCtx.drawImage(floors[floor].img, 0, 0);
+  if (floor.img) {
+    canvasCtx.drawImage(floor.img, 0, 0);
+  }
+
+  for (let i = 0; i < floor.nodes.length; i++) {
+    const node = floor.nodes[i];
+    canvasCtx.strokeStyle = getNodeTypeStrokeStyle(node.type);
+    canvasCtx.fillStyle = getNodeTypeFillStyle(node.type);
+    canvasCtx.beginPath();
+    canvasCtx.ellipse(node.x, node.y, NODE_SIZE, NODE_SIZE, 0, 0, 2 * Math.PI);
+    canvasCtx.stroke();
+    canvasCtx.fill();
   }
 }
 
@@ -249,9 +460,13 @@ function handleFloorImage(e) {
 $(document).ready(function() {
   canvas = $('#contentCanvas');
   canvasCtx = document.getElementById('contentCanvas').getContext('2d');
+  canvas.click(handleCanvasClick);
+  canvas.mousedown(handleCanvasMouseDown);
+  canvas.mouseup(handleCanvasMouseUp);
+  canvas.mousemove(handleCanvasMouseMove);
   handleResize();
 
-  showCurrentMenu();
+  setCurrentMenu(MENU_BASE);
   highlightCurrentTool();
 
   $('#toolbar').draggable({ containment: '#toolbar-area' });
@@ -276,7 +491,6 @@ $(document).ready(function() {
 
   // Altering floors
   $('#base-floors').on('click', '.floor-change', handleFloorChange);
-
 });
 
 // Bind resize function
